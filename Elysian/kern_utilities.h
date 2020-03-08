5056330cd5c57edebedba5179122ed297f965ea3
chr1s0x1 <chr1spwn3d@gmail.com>
2020-03-08 18:55:30 -0400

    add patchfinder (needs to be updated for ios 13)

--- Elysian/kern_utilities.h
+++ Elysian/kern_utilities.h
@@ -6,10 +6,17 @@
 //  Copyright © 2020 chr1s_0x1. All rights reserved.
 //
 
+/* A lot of this code is from the unc0ver team (gotta give credit :) ))*/
+
 #ifndef kern_utilities_h
 #define kern_utilities_h
 
 #include <stdio.h>
 #import "jailbreak.h"
+
+kptr_t = ptr_t;
+
+kptr_t find_kern_cred_addr(void);
+
 
 #endif /* kern_utilities_h */
