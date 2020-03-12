48c035a23d9cd2307dbdad97dcb697924d084418
chr1s0x1 <chr1spwn3d@gmail.com>
2020-03-12 15:05:51 -0400

    stuff
    forgot this

--- Elysian/IOSurface_stuff.h
+++ Elysian/IOSurface_stuff.h
@@ -15,7 +15,7 @@
 
 #import <IOKit/IOKitLib.h>
 #import <mach/mach.h>
-
+#import "offsets.h"
 
 
 #define IOSurfaceRootUserClient_create_surface_selector 6
--- Elysian/kernel_memory.h
+++ Elysian/kernel_memory.h
@@ -13,7 +13,7 @@
 #import <stdbool.h>
 #import <stdlib.h>
 #import <mach/mach.h>
-
+#import "offsets.h"
 
 kern_return_t mach_vm_allocate(vm_map_t target, mach_vm_address_t *address, mach_vm_size_t size, int flags);
 kern_return_t mach_vm_read_overwrite(vm_map_t target_task, mach_vm_address_t address, mach_vm_size_t size, mach_vm_address_t data, mach_vm_size_t *outsize);
